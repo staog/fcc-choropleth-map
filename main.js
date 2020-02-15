5351cc2e6c69648ae2cc6eb97ec49ebb958f9103
Milan V. Kecojević <staog@gmx.com>
2020-02-15 23:10:44 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,2 @@
-console.log(data);
+const education = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+const county = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
