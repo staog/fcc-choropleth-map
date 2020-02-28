92c36a9330365f7206f3dcca96e93231c188a8c8
Milan V. Kecojević <staog@gmx.com>
2020-02-28 16:25:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,42 +1,42 @@
-const educationDataSet = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
-const countyDataSet = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
-
-const files = [educationDataSet, countyDataSet];
-
-Promise.all(files.map(url => d3.json(url))).then(values => choroplethmap(values));
-
-function choroplethmap(data) {
-  
-  const counties = topojson.feature(data[1], data[1].objects.counties);
-
-  const width = 960;
-  const height = 600;
-  const margin = {
+const width = 960;
+const height = 600;
+const margin = {
     top: 20,
     right: 20,
     bottom: 20,
     left: 20
   }
 
-  const innerWidth = width - margin.left - margin.right;
-  const innerHeight = height - margin.top - margin.bottom;
+const main = d3.select("#main");
 
-  const main = d3.select("#main");
-
-  main.append("h1")
-      .attr("id", "title")
-      .text("United States Educational Attainment");
+main.append("h1")
+    .attr("id", "title")
+    .text("United States Educational Attainment");
   
-  main.append("h3")
-      .attr("id", "description")
-      .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
+main.append("h3")
+    .attr("id", "description")
+    .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
 
-  const svg = main.append("svg")
-                  .attr("align", "centre")
-                  .attr("width", width)
-                  .attr("height", height);
 
-  const projection = d3.geoMercator();
-  const pathGenereator = d3.geoPath(projection(projection));
+var svg = main.append("svg")
+              .attr("align", "centre")
+              .attr("width", width)
+              .attr("height", height);
 
-};
+var path = d3.geoPath();
+
+d3.json("https://d3js.org/us-10m.v1.json", function(error, us) {
+  if (error) throw error;
+
+  svg.append("g")
+     .attr("class", "counties")
+     .selectAll("path")
+     .data(topojson.feature(us, us.objects.counties).features)
+     .enter().append("path")
+     .attr("d", path)
+     .attr("fill", "darkcyan");
+
+  svg.append("path")
+      .attr("class", "county-borders")
+      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b; })));
+});
