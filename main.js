aedecc233c415a129ccbad1e32a790ea0601e338
Milan V. Kecojević <staog@gmx.com>
2020-03-02 23:33:11 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,48 @@
-const width = 960;
-const height = 600;
-const margin = {
+const educationDataSet = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/for_user_education.json";
+const countyDataSet = "https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json";
+
+const files = [educationDataSet, countyDataSet];
+
+Promise.all(files.map(url => d3.json(url))).then(values => choroplethmap(values));
+
+function choroplethmap(data) {
+  
+  console.log(data)
+  
+  const width = 960;
+  const height = 600;
+  const margin = {
     top: 20,
     right: 20,
     bottom: 20,
     left: 20
   }
 
-const main = d3.select("#main");
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
 
-main.append("h1")
-    .attr("id", "title")
-    .text("United States Educational Attainment");
+  const main = d3.select("#main");
+
+  main.append("h1")
+      .attr("id", "title")
+      .text("United States Educational Attainment");
   
-main.append("h3")
-    .attr("id", "description")
-    .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
+  main.append("h3")
+      .attr("id", "description")
+      .text("Percentage of adults age 25 and older with a bachelor's degree or higher (2010-2014)");
 
+  const svg = main.append("svg")
+                  .attr("align", "centre")
+                  .attr("width", width)
+                  .attr("height", height);
 
-var svg = main.append("svg")
-              .attr("align", "centre")
-              .attr("width", width)
-              .attr("height", height);
-
-var path = d3.geoPath();
-
-d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/counties.json", us => {
+  const projection = d3.geoMercator();
+  const path = d3.geoPath();
 
   svg.append("g")
      .attr("class", "counties")
      .selectAll("path")
-     .data(topojson.feature(us, us.objects.counties).features)
+     .data(topojson.feature(data[1], data[1].objects.counties).features)
      .enter()
      .append("path")
      .attr("d", path)
@@ -38,5 +50,7 @@ d3.json("https://cdn.freecodecamp.org/testable-projects-fcc/data/choropleth_map/
 
   svg.append("path")
       .attr("class", "county-borders")
-      .attr("d", path(topojson.mesh(us, us.objects.counties, (a, b) => a !== b)));
-});
+      .attr("d", path(topojson.mesh(data[1], data[1].objects.counties, (a, b) => a !== b)));
+  
+};
+
